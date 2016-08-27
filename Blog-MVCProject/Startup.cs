41be338eafdca93686b333fcc1d1fa3bf84ec485
Gornakoff <nikolaygornakov@yahoo.com>
2016-08-27 12:28:43 +0300

    Basic Configuration
    - Basic configuration
    - Automatic Migrations Enabled
    - Mapped DB to the project
    - Password Validation simplified for devs purposes

---
+++ Blog-MVCProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blog_MVCProject.Startup))]
+namespace Blog_MVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
